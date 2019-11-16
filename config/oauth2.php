e9f331f15ffb7154d25a00a81e62a973b88e8e4a
Emeka Augustine <cafafanscoders@gmail.com>
2019-11-16 11:20:23 +0800

    Fix minor bugs in Excepton handler

---
+++ config/oauth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'grant_types' => [
+        'password'           => env('PASSWORD_GRANT', 'password'),
+        'refresh_token'      => env('REFRESH_TOKEN_GRANT', 'refresh_token'),
+        'client_credentials' => env('CLIENT_CREDENTIALS_GRANT', 'client_credentials'),
+        'authorization_code' => env('AUTHORIZATION_CODE_GRANT', 'authorization_code'),
+        'implicit'           => env('IMPLICIT_GRANT', 'implicit'),
+    ],
+
+    'credentials' => [
+        'client_id'         => env('CLIENT_ID'),
+        'client_secret'     => env('CLIENT_SECRET'),
+        'redirect_uri'      => env('REDIRECT_URI'),
+        'response_type'     => env('RESPONSE_TYPE'),
+        'scope'             => env('SCOPE'),
+    ],
+];
